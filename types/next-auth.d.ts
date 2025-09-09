c0118a57310292a7559990b148fb2af3b0aabfb8
Anas Muhammad <ans.2012.1.25@gmail.com>
2025-09-09 14:15:02 +0300

    Version 1.1

---
+++ types/next-auth.d.ts
@@ -0,0 +1,23 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import NextAuth, { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user?: {
+      id: string; // 👈 هنا نضيف الـ userId من الـ DB
+    } & DefaultSession["user"];
+  }
+
+  interface User {
+    id: string;
+    name?: string | null;
+    email?: string | null;
+    image?: string | null;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+  }
+}
