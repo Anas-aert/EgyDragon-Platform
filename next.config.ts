a0ca5436e40e20e6cadc01177cc32e3f90788157
Anas Muhammad <ans.2012.1.25@gmail.com>
2025-09-14 18:47:18 +0300

    Version 1.1

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,8 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "lh3.googleusercontent.com" }, // صور Google
       { protocol: "https", hostname: "avatars.githubusercontent.com" }, // صور GitHub
       { protocol: "https", hostname: "images.unsplash.com" }, // Unsplash
+      { protocol: "https", hostname: "*.googlesyndication.com" }, // صور إعلانات Google
+      { protocol: "https", hostname: "*.googleusercontent.com" }, // صور مرتبطة بحساب Google
     ],
   },
   eslint: {
@@ -20,12 +22,23 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: `
               default-src 'self';
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.vercel-insights.com;
+              script-src 'self' 'unsafe-inline' 'unsafe-eval'
+                https://*.vercel-insights.com
+                https://pagead2.googlesyndication.com
+                https://googleads.g.doubleclick.net
+                https://tpc.googlesyndication.com;
               style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-              img-src 'self' data: https://lh3.googleusercontent.com https://avatars.githubusercontent.com https://images.unsplash.com;
+              img-src 'self' data:
+                https://lh3.googleusercontent.com
+                https://avatars.githubusercontent.com
+                https://images.unsplash.com
+                https://*.googlesyndication.com
+                https://*.googleusercontent.com;
               font-src 'self' https://fonts.gstatic.com;
               connect-src 'self' https://*.vercel-insights.com;
-              frame-src 'self';
+              frame-src 'self'
+                https://googleads.g.doubleclick.net
+                https://pagead2.googlesyndication.com;
             `.replace(/\s{2,}/g, " "),
           },
           {
