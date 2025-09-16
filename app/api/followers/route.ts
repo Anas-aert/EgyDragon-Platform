c4c07ecabd289aa8f9db11f53b98cde90aacd6ba
Anas Muhammad <ans.2012.1.25@gmail.com>
2025-09-16 10:17:13 +0300

    Ver 1.1

--- app/api/followers/route.ts
+++ app/api/followers/route.ts
@@ -29,15 +29,19 @@ export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = followSchema.safeParse(body);
 
+  console.log(validation)
+  console.log(validation.success)
+  console.log(body)
+  
   if (!validation.success) {
     return NextResponse.json(
       { error: validation.error.issues[0].message },
       { status: 400 }
     );
   }
-
+  
   const { userId, postUserId } = body;
-
+  
   const existingFollow = await prisma.follow.findUnique({
     where: {
       followerId_followingId: {
@@ -46,18 +50,18 @@ export async function POST(request: NextRequest) {
       },
     },
   });
-
+  
   if (existingFollow) {
     return NextResponse.json({ message: "Already following" }, { status: 400 });
   }
-
+  
   const follow = await prisma.follow.create({
     data: {
       followerId: userId,
       followingId: postUserId,
     },
   });
-
+  
   return NextResponse.json(follow, { status: 200 });
 }
 
@@ -65,7 +69,11 @@ export async function POST(request: NextRequest) {
 export async function DELETE(request: NextRequest) {
   const body = await request.json();
   const validation = followSchema.safeParse(body);
-
+  
+  console.log(validation)
+  console.log(validation.success)
+  console.log(body)
+  
   if (!validation.success) {
     return NextResponse.json(
       { error: validation.error.issues[0].message },
