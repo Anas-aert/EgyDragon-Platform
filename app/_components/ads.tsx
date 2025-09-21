fc47c26ebc07440521e9be649025724b3203c2af
Anas Muhammad <ans.2012.1.25@gmail.com>
2025-09-21 09:14:09 +0300

    Version 1.1

--- app/_components/ads.tsx
+++ app/_components/ads.tsx
@@ -5,7 +5,7 @@ export default function VignetteLoader() {
   useEffect(() => {
     const script = document.createElement("script");
     script.id = "monetag-script";
-    script.src = "//magnificentmanlyyeast.com/0d/8c/2e/0d8c2e5a015a7a0d381cfdfea680d473.js";
+    script.src = "https://magnificentmanlyyeast.com/0d/8c/2e/0d8c2e5a015a7a0d381cfdfea680d473.js";
     script.async = true;
     document.body.appendChild(script);
 
