a697f1fc76d8c4bc7474650993b624ab3a869c68
Anas Muhammad <ans.2012.1.25@gmail.com>
2025-10-01 18:27:30 +0300

    Version 1.1

--- app/feed/loading.tsx
+++ app/feed/loading.tsx
@@ -1,5 +1,20 @@
-export default async function Loading() {
-    return (
-        <div className="loader"></div>
-    )
-}
+import React from "react";
+import "../globals.css"; // لو ملفك في نفس المجلد الرئيسي
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <div className="box box0"><div /></div>
+      <div className="box box1"><div /></div>
+      <div className="box box2"><div /></div>
+      <div className="box box3"><div /></div>
+      <div className="box box4"><div /></div>
+      <div className="box box5"><div /></div>
+      <div className="box box6"><div /></div>
+      <div className="box box7"><div /></div>
+      <div className="ground"><div /></div>
+    </div>
+  );
+};
+
+export default Loader;
